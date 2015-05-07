d2d4991a009b61f82a6674050793ff29a92a871c
Ney Estrabelli <neyestrabelli@gmail.com>
2015-05-07 13:38:31 -0300

    Fix maxlength with mDec

--- autoNumeric.js
+++ autoNumeric.js
@@ -660,7 +660,7 @@
         * check maxlength from field
         */
         checkMaxLength: function (e) {
-
+            var cCode = String.fromCharCode(this.which);
             var m_length  = this.that.maxLength;
             if(m_length <= 0) return false;
 
@@ -677,11 +677,17 @@
 
             var re = $settings.aDec;
             if(myValue.match(re)){
-              if( myValue.length >= m_length ) {
+              if( myValue.length >= (m_length + $settings.mDec)) {
                 return true;
               }
             } else {
-              if( myValue.length >= maxLength ) {
+              if (cCode === $settings.aDec ||
+                 ($settings.altDec && cCode === $settings.altDec) ||
+                 ((cCode === '.' || cCode === ','))) {
+                      return false;
+              }
+
+              if( myValue.length >= maxLength) {
                 return true;
               }
             }

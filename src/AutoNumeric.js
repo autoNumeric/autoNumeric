61183683c98e859fc900a4cf6632cfef8255cc7e
beastlike <beastlike@users.noreply.github.com>
2017-05-12 20:06:01 -0400

    Fix issue #440 Pasting does not work in IE11
    changed paste code to fall back to window.clipboardData if it exists (ie11 localhost/intranet)

--- src/AutoNumeric.js
+++ src/AutoNumeric.js
@@ -5312,7 +5312,8 @@ class AutoNumeric {
         // The event is prevented by default, since otherwise the user would be able to paste invalid characters into the input
         e.preventDefault();
 
-        let rawPastedText = e.clipboardData.getData('text/plain');
+        // fall back to window.clipboardData if it exists (ie11 localhost/intranet)
+        let rawPastedText = (window.clipboardData ? window.clipboardData.getData('text') : e.clipboardData.getData('text/plain'));
 
         // 0. Special case if the user has selected all the input text before pasting
         const initialFormattedValue = AutoNumericHelper.getElementValue(e.target);

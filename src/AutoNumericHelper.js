3b175a9e67482f3add0f08ca20f55edfca1bd546
benny.pu <benny.pu@shareworks.cn>
2019-05-27 17:51:36 +0800

    feature: when event.key === 'Process' this Control does not work

--- src/AutoNumericHelper.js
+++ src/AutoNumericHelper.js
@@ -370,7 +370,7 @@ export default class AutoNumericHelper {
      */
     static character(event) {
         let result;
-        if (event.key === 'Unidentified' || event.key === void(0) || this.isSeleniumBot()) {
+        if (event.key === 'Unidentified' || event.key === void(0) || this.isSeleniumBot() || event.key === 'Process') {
             //XXX The selenium geckodriver do not understand `event.key`, hence when using it, we need to rely on the old deprecated `keyCode` attribute, cf. upstream issue https://github.com/mozilla/geckodriver/issues/440
             // Use the old deprecated keyCode property, if the new `key` one is not supported
             const keyCode = this.keyCodeNumber(event);

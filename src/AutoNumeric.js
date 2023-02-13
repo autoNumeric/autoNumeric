be584c47611eb3cc790951b0f11481b065992f98
Melloware <mellowaredev@gmail.com>
2023-02-13 14:53:52 -0500

    Fix #734: Fire change event onBlur only if number is changed (#735)
    * Fix #734: Fire change event onBlur only if number is changed
    
    * Fix #734: Fire change event onBlur only if number is changed

--- src/AutoNumeric.js
+++ src/AutoNumeric.js
@@ -7426,7 +7426,7 @@ To solve that, you'd need to either set \`decimalPlacesRawValue\` to \`null\`, o
         this.isEditing = false;
 
         // Send a `change` event if the raw value has been changed since the last focus or 'enter' validation
-        if (this.rawValue !== this.rawValueOnFocus) {
+        if (AutoNumericHelper.trimPaddedZerosFromDecimalPlaces(this.rawValue) !== AutoNumericHelper.trimPaddedZerosFromDecimalPlaces(this.rawValueOnFocus)) {
             this._triggerEvent(AutoNumeric.events.native.change, e.target);
         }
 
